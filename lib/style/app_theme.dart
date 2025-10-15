9dd6ac2ccc2788880e343e0cbdabc2946d9742eb
Md. Pieash Ahmed <pieash9@gmail.com>
2025-10-15 13:07:33 +0600

    Refactor AppTheme to enhance dark theme configuration and improve readability

--- lib/style/app_theme.dart
+++ lib/style/app_theme.dart
@@ -2,14 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:portfolio/style/app_colors.dart';
 
 class AppTheme {
-  static ThemeData get dark => ThemeData(
-    colorScheme: ColorScheme.dark(
-      primary: AppColors.primaryColor,
-      surface: AppColors.darkBackgroundColor,
-      onSurface: AppColors.gray[100]!,
-    ),
-    useMaterial3: true,
-    scaffoldBackgroundColor: AppColors.darkBackgroundColor,
-    appBarTheme: AppBarTheme(color: AppColors.gray[900]),
-  );
+  ThemeData get dark {
+    return _getThemeData(
+      colorScheme: ColorScheme.dark(
+        primary: AppColors.primaryColor,
+        background: AppColors.darkBackgroundColor,
+        surface: AppColors.gray[850]!,
+        outline: AppColors.gray[800],
+        outlineVariant: AppColors.gray[700],
+        onBackground: AppColors.gray[100],
+        onSurface: AppColors.gray[100]!,
+        onSurfaceVariant: AppColors.gray[400],
+        tertiary: AppColors.gray[900],
+      ),
+      scaffoldBackgroundColor: AppColors.darkBackgroundColor,
+      elevatedButtonTextStyle: _darkElevatedButtonTextStyle,
+      outlineButtonTextStyle: _darkOutlineButtonTextStyle,
+      useMaterial3: true,
+      appBarTheme: AppBarTheme(color: AppColors.gray[900]!.withOpacity(0.3)),
+    );
+  }
 }
